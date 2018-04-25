3c1c21b6558e345a888ac9468da5645620dbb873
Ryan Bacon <tservo@tservo.org>
2018-04-25 15:47:25 -0700

    JSON tools working Started working on layout.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,62 @@
 package com.udacity.sandwichclub.utils;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+
 public class JsonUtils {
+    private static String TAG = JsonUtils.class.getSimpleName();
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Log.d(TAG,json);
+        Sandwich sandwich;
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+
+            // try to get name
+            JSONObject nameObject = jsonObject.getJSONObject("name");
+            String mainName  = nameObject.getString("mainName");
+
+
+            // iterate over the JSONArray to make our alsoKnownAs ArrayList
+            JSONArray aka = nameObject.getJSONArray("alsoKnownAs");
+            ArrayList<String> alsoKnownAs = new ArrayList<>(aka.length());
+            for (int i = 0; i < aka.length(); i++) {
+                alsoKnownAs.add(aka.getString(i));
+            }
+
+            // get the simple strings
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+
+            // get the ingredients array
+            JSONArray ingredientsJSON = jsonObject.getJSONArray("ingredients");
+            ArrayList<String> ingredients = new ArrayList<>(ingredientsJSON.length());
+            for (int i = 0; i < ingredientsJSON.length(); i++) {
+                ingredients.add(ingredientsJSON.getString(i));
+            }
+
+            sandwich = new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
+        } catch (JSONException e) {
+            Log.e(TAG,"Could not parse JSON "+json);
+            return null;
+        }
+        // get main name:
+
+        return sandwich;
+
     }
 }
