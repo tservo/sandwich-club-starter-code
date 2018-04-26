fce9f057a89bd446ee81ff82c432ea28ff771e19
Ryan Bacon <tservo@tservo.org>
2018-04-25 17:11:19 -0700

    Make some aethetic changes.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +62,38 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Helper method to join a list of strings into one string.
+     * @param delimiter - used to join strings
+     * @param stringList - list of strings to join
+     * @return combined string using delimiter
+     */
+    private String join(String delimiter, List<String> stringList) {
 
+        StringBuilder outString = new StringBuilder();
+        for (int i=0; i < stringList.size(); i++) {
+            outString.append(stringList.get(i));
+            if (i < stringList.size() - 1) {
+                // there's more to come so use delimiter
+                outString.append(delimiter);
+            }
+        }
+
+        return outString.toString();
+    }
+
+    private void populateUI(Sandwich sandwich) {
+        TextView tvMainName = findViewById(R.id.main_name_tv);
+        TextView tvAlsoKnownAs =  findViewById(R.id.also_known_tv);
+        TextView tvIngredients =  findViewById(R.id.ingredients_tv);
+        TextView tvDescription =  findViewById(R.id.description_tv);
+        TextView tvPlaceOfOrigin =  findViewById(R.id.origin_tv);
+
+
+        tvMainName.setText(sandwich.getMainName());
+        tvAlsoKnownAs.setText(join(", ",sandwich.getAlsoKnownAs()));
+        tvIngredients.setText(join(", ",sandwich.getIngredients()));
+        tvDescription.setText(sandwich.getDescription());
+        tvPlaceOfOrigin.setText(sandwich.getPlaceOfOrigin());
     }
 }
